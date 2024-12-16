3dcbf910b2cf5cab1318c8c2cf18a568c4e62bca
Pepe <pepeglezabad@gmail.com>
2024-12-16 17:09:11 +0100

    12. Grammar update - added pow operation

--- Parser.hs
+++ Parser.hs
@@ -8,10 +8,15 @@ Grammar
 expr    : term ((PLUS | MINUS) term)* -> BinOpNode [+, -]
 term    : factor ((MUL | DIV) factor)* -> BinOpNode [*, /]
 
-factor  : INT | FLOAT -> NumNode
-        : (PLUS | MINUS) factor
+factor  : (PLUS | MINUS) factor
+        : power
+
+power   : atom (POW factor)*
+
+atom    : INT | FLOAT -> NumNode
         : LPAREN expr RPAREN
 -}
+
 -- Parser: Parses a list of tokens into an AST
 parse :: [Token] -> ParseResult
 parse tokens = do
@@ -36,21 +41,15 @@ parse_expr tokens = do
         (res_left, [])
 
 build_expr_ast :: AST -> [Token] -> (ParseResult, [Token])
-build_expr_ast left (TPlus:tokens) = do
-    let (res_right, new_tokens) = parse_term tokens
-    if is_success res_right then
-        build_expr_ast (BinOpNode TPlus left (get_ast res_right)) new_tokens
+build_expr_ast left (tok:tokens) = do
+    if tok == TPlus || tok == TMinus then do
+        let (res_right, new_tokens) = parse_term tokens
+        if is_success res_right then
+            build_expr_ast (BinOpNode tok left (get_ast res_right)) new_tokens
+        else
+            (res_right, [])
     else
-        (res_right, [])
-
-build_expr_ast left (TMinus:tokens) = do
-    let (res_right, new_tokens) = parse_term tokens
-    if is_success res_right then
-        build_expr_ast (BinOpNode TMinus left (get_ast res_right)) new_tokens
-    else
-        (res_right, [])
-
-build_expr_ast left tokens = (Success left, tokens)
+        (Success left, (tok:tokens))
 
 -- Parse a term (supports multiplication and division)
 parse_term :: [Token] -> (ParseResult, [Token])
@@ -62,26 +61,35 @@ parse_term tokens = do
         (res_left, [])
 
 build_term_ast :: AST -> [Token] -> (ParseResult, [Token])
-build_term_ast left (TMult:tokens) = do
-    let (res_right, new_tokens) = parse_factor tokens
+build_term_ast left (tok:tokens) = do
+    if tok == TMult || tok == TDiv then do
+        let (res_right, new_tokens) = parse_factor tokens
+        if is_success res_right then
+            build_term_ast (BinOpNode tok left (get_ast res_right)) new_tokens
+        else
+            (res_right, [])
+    else
+        (Success left, (tok:tokens))
+
+parse_power :: [Token] -> (ParseResult, [Token])
+parse_power tokens = do
+    let (res_left, new_tokens) = parse_atom tokens
+    if is_success res_left then
+        build_power_ast (get_ast res_left) new_tokens
+    else
+        (res_left, [])
+
+build_power_ast :: AST -> [Token] -> (ParseResult, [Token])
+build_power_ast left (TPow:tokens) = do
+    let (res_right, new_tokens) = parse_atom tokens
     if is_success res_right then
-        build_term_ast (BinOpNode TMult left (get_ast res_right)) new_tokens
+        build_power_ast (BinOpNode TPow left (get_ast res_right)) new_tokens
     else
         (res_right, [])
 
-build_term_ast left (TDiv:tokens) = do
-    let (res_right, new_tokens) = parse_factor tokens
-    if is_success res_right then
-        build_term_ast (BinOpNode TDiv left (get_ast res_right)) new_tokens
-    else
-        (res_right, [])
-build_term_ast left tokens = (Success left, tokens)
+build_power_ast left tokens = (Success left, tokens)
 
--- Parse a factor (supports integers)
 parse_factor :: [Token] -> (ParseResult, [Token])
-parse_factor ((TInt n):tokens) = (Success (NumNode (TInt n)), tokens)
-parse_factor ((TFloat n):tokens) = (Success (NumNode (TFloat n)), tokens)
-
 parse_factor (TPlus:tokens) = do
     let (res, new_tokens) = parse_factor tokens
     if is_success res then
@@ -96,8 +104,13 @@ parse_factor (TMinus:tokens) = do
     else
         (res, new_tokens)
 
-parse_factor (TLParen:[]) = (Failure (InvalidSyntaxError "Expected a \')\'"), [])
-parse_factor (TLParen:tokens) = do
+parse_factor tokens = parse_power tokens
+
+parse_atom :: [Token] -> (ParseResult, [Token])
+parse_atom ((TInt n):tokens) = (Success (NumNode (TInt n)), tokens)
+parse_atom ((TFloat n):tokens) = (Success (NumNode (TFloat n)), tokens)
+parse_atom (TLParen:[]) = (Failure (InvalidSyntaxError "Expected a \')\'"), [])
+parse_atom (TLParen:tokens) = do
     let (res, new_tokens) = parse_expr tokens
     if new_tokens == [] then
         (Failure (InvalidSyntaxError "Expected expression"), [])
@@ -107,9 +120,9 @@ parse_factor (TLParen:tokens) = do
         else
             (Failure (InvalidSyntaxError "Expected a \')\'"), [])
 
-parse_factor (TRParen:[]) = (Failure (InvalidSyntaxError "Unexpected \')\'"), [])
+parse_atom (TRParen:[]) = (Failure (InvalidSyntaxError "Unexpected \')\'"), [])
 
-parse_factor _ = (Failure (InvalidSyntaxError "Expected a number"), [])
+parse_atom _ = (Failure (InvalidSyntaxError "Expected a '+', '-', '(' or number)"), [])
 
 -- Helper function for ParseResult
 is_success :: ParseResult -> Bool
